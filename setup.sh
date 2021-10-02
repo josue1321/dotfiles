de82a3822eefd22101cf846f5e711f8a15b8d72b
Josué <80179969+josue1321@users.noreply.github.com>
2021-10-02 15:57:29 -0300

    ajuste no setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 
 #! /bin/bash
 
-echo "Iniciando Setup"
+echo "Iniciando Setup\n"
+
 sudo pacman -S bspwm sxhkd rofi nitrogen rxvt-unicode
-#cd /tmp/
-#git clone https://aur.archlinux.org/polybar
-#cd /tmp/polybar
-#makepkg -si
-#git clone https://aur.archlinux.org/picom-jonaburg-git
-#cd /tmp/picom-jonaburg-git
-#makepkg -si
+
+cd /tmp/
+git clone https://aur.archlinux.org/polybar
+cd /tmp/polybar
+makepkg -si
+
+git clone https://aur.archlinux.org/picom-jonaburg-git
+cd /tmp/picom-jonaburg-git
+makepkg -si
+
 cd $HOME
 mkdir $HOME/.config/picom
 cp /etc/xdg/picom.conf.example $HOME/.config/picom/picom.conf
 mv $HOME/dotfiles/bspwm $HOME/.config/ && mv $HOME/dotfiles/sxhkd $HOME/.config/ && mv $HOME/dotfiles/polybar $HOME/.config/ && mv $HOME/dotfiles/urxvt/.Xresources $HOME
 sudo pacman -Rs $(pacman -Qqdt)
-echo "Setup completo"
+
+echo "\nSetup completo"
